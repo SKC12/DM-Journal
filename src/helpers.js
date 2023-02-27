862612df39751218296cde849fd9d6999484163e
SKC12 <stephancopetti@hotmail.com>
2023-02-27 17:46:16 -0300

    refactor CampaignInfo with Campaign class

--- src/helpers.js
+++ src/helpers.js
@@ -3,78 +3,9 @@ import {
   collection,
   doc,
   setDoc,
-  query,
-  where,
   getDocs,
   deleteDoc,
 } from "firebase/firestore";
-
-//Load campaigns array from database
-async function loadCampaignsFromDatabase(userID) {
-  let campArray = [];
-  try {
-    const query = await getDocs(
-      collection(db, "users/" + userID + "/campaigns")
-    );
-    query.forEach((doc) => {
-      campArray.push(doc.data());
-    });
-  } catch (e) {
-    console.log(e);
-    throw e;
-  }
-
-  //console.log("LOADING CAMPAIGNS FROM DB");
-
-  return campArray;
-}
-
-async function searchFirebaseForCampaignName(userID, name) {
-  try {
-    const q = query(
-      collection(db, "users/" + userID + "/campaigns"),
-      where("name", "==", name)
-    );
-    const docs = await getDocs(q);
-    return docs;
-  } catch (e) {
-    console.log(e);
-    throw e;
-  }
-}
-
-async function writeCampaignToFirebase(userID, campaignName, campaign) {
-  try {
-    await setDoc(
-      doc(db, "users/" + userID + "/campaigns", campaignName),
-      campaign
-    );
-  } catch (e) {
-    console.log(e);
-    throw e;
-  }
-}
-
-async function deleteCampaignFromFirebase(userID, name) {
-  try {
-    const q = query(
-      collection(db, "users/" + userID + "/campaigns/" + name + "/sessions")
-    );
-    const docs = await getDocs(q);
-
-    const deletions = [];
-    docs.forEach((doc) => {
-      deletions.push(deleteDoc(doc.ref));
-    });
-
-    Promise.all(deletions).then();
-
-    await deleteDoc(doc(db, "users/" + userID + "/campaigns", name));
-  } catch (e) {
-    console.log(e);
-    throw e;
-  }
-}
 
 //Loads category array from Firebase
 async function loadFromFirebase(categoryString, userID, campName) {
